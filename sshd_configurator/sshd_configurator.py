fd8cdb8a375ca039a4e908697d785bc3d3b90576
jakeogh <github.com@v6y.net>
2019-02-17 04:13:08 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import daemon
-from sshd_configurator_daemon import sshd_configurator_daemon
+from .sshd_configurator_daemon import sshd_configurator_daemon
 
 if __name__ == '__main__':
     with daemon.DaemonContext():
