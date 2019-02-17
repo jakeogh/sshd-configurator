aa1a791abe6e599e8e9a6183a31fe32d8de56c98
jakeogh <github.com@v6y.net>
2019-02-17 04:16:58 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+import click
 import daemon
 from .sshd_configurator_daemon import sshd_configurator_daemon
 
-
-def sshd_configurator():
+@click.command()
+@click.argument('interface', nargs=1)
+@click.option('--daemon', is_flag=True)
+@click.option('--sshd-config', is_flag=False, default='/etc/ssh/sshd_config')
+def sshd_configurator(interface, daemon, sshd_config):
     with daemon.DaemonContext():
-        sshd_configurator_daemon()
+        sshd_configurator_daemon(interface=interface, daemon=daemin, sshd_config=sshd_config)
 
 
 if __name__ == '__main__':
