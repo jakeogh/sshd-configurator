7b2d2c02f76532f7676c845e99a51711cd1b13ba
jakeogh <github.com@v6y.net>
2019-02-17 04:45:48 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
 import click
-import daemon
+import attr
+from daemonize import Daemonize
 from .sshd_configurator_daemon import sshd_configurator_daemon
+
+global pidfile
+pidfile = "/run/sshd-configurator.pid"
+
+@attr.s(auto_attribs=True)
+class SSHD_CONFIGURATOR():
+    interface: str
+    daemon: bool
+    sshd_config: str
+
+    def run(self):
+        sshd_configurator_daemon(interface=self.interface, daemon=self.daemon, sshd_config=self.sshd_config)
+
 
 @click.command()
 @click.argument('interface', nargs=1)
-@click.option('--daemon', 'be_daemon', is_flag=True)
+@click.option('--daemon', is_flag=True)
 @click.option('--sshd-config', is_flag=False, default='/etc/ssh/sshd_config')
-def sshd_configurator(interface, be_daemon, sshd_config):
-    print("starting with be_daemon:", be_daemon)
-    if be_daemon:
-        with daemon.DaemonContext():
-            sshd_configurator_daemon(interface=interface, daemon=be_daemon, sshd_config=sshd_config)
-            print("exiting")
-    else:
-        sshd_configurator_daemon(interface=interface, daemon=be_daemon, sshd_config=sshd_config)
+def sshd_configurator(interface, daemon, sshd_config):
+    sshd_configurator_obj = SSHD_CONFIGURATOR(interface=interface, daemon=daemon, sshd_config=sshd_config)
+    daemon = Daemonize(app="ssh_configurator", pid=pidfile, action=sshd_configurator_obj.run)
+    daemon.start()
 
 
 if __name__ == '__main__':
