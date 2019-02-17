057436216a8bff9be2e7de045aa5451bb045c9eb
jakeogh <github.com@v6y.net>
2019-02-17 04:19:56 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -6,11 +6,14 @@ from .sshd_configurator_daemon import sshd_configurator_daemon
 
 @click.command()
 @click.argument('interface', nargs=1)
-@click.option('--daemon', is_flag=True)
+@click.option('--daemon', name='be_daemin', is_flag=True)
 @click.option('--sshd-config', is_flag=False, default='/etc/ssh/sshd_config')
-def sshd_configurator(interface, daemon, sshd_config):
-    with daemon.DaemonContext():
-        sshd_configurator_daemon(interface=interface, daemon=daemin, sshd_config=sshd_config)
+def sshd_configurator(interface, be_daemon, sshd_config):
+    if daemon:
+        with daemon.DaemonContext():
+            sshd_configurator_daemon(interface=interface, daemon=be_daemon, sshd_config=sshd_config)
+    else:
+        sshd_configurator_daemon(interface=interface, daemon=be_daemon, sshd_config=sshd_config)
 
 
 if __name__ == '__main__':
