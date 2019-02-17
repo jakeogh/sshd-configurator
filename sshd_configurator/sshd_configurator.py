5d590594a3253c9ffdce23930d5304f546b3e3b6
jakeogh <github.com@v6y.net>
2019-02-17 02:50:38 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -54,7 +54,14 @@ def sshd_configurator(interface, sshd_config):
     assert listen_address
     ssh_rule = 'ListenAddress ' + listen_address + ':22\n'
     ssh_rule = ssh_rule.encode('utf8')
-    write_unique_line_to_file(line=ssh_rule, file_to_write=sshd_config)
+    try:
+        write_unique_line_to_file(line=ssh_rule, file_to_write=sshd_config)
+    except PermissionError as e:
+        print("ERROR: Unable to write to", sshd_config, file=sys.stderr)
+        print("ERROR: Run \"chattr -i " + sshd_config + "\"", file=sys.stderr)
+        quit(1)
+
+
     try:
         with open('/etc/conf.d/sshd', 'r') as fh:
             fhr = filter(None, (line.strip() for line in fh))
