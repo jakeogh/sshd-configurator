8e46bfc148de07ea4e3d84d5f32d0371a7858bc6
jakeogh <github.com@v6y.net>
2022-04-05 11:01:42 -0700

    auto-commit

--- sshd_configurator/sshd_configurator.py
+++ sshd_configurator/sshd_configurator.py
@@ -1,52 +1,65 @@
 #!/usr/bin/env python3
+# pylint: disable=R0903  # too few public methods
 
-import click
 import logging
+import sys
+
 import attr
+import click
 from daemonize import Daemonize
+
 from .sshd_configurator_daemon import sshd_configurator_daemon
 
 
 def warn_confd_sshd(logger):
-    logger.error("\nERROR: /etc/conf.d/sshd is not configured to depend on sshd-configurator")
-    logger.error("ERROR: add rc_need=\"sshd-configurator\" to /etc/conf.d/sshd")
-    quit(1)
+    logger.error(
+        "\nERROR: /etc/conf.d/sshd is not configured to depend on sshd-configurator"
+    )
+    logger.error('ERROR: add rc_need="sshd-configurator" to /etc/conf.d/sshd')
+    sys.exit(1)
 
 
 def warn_confd_sshd_configurator(logger, interface):
-    logger.error("\nERROR: /etc/conf.d/sshd-configurator is not configured to depend on", interface)
-    logger.error("ERROR: add rc_need=\"net." + interface + "\" to /etc/conf.d/sshd-configurator")
-    quit(1)
+    logger.error(
+        "\nERROR: /etc/conf.d/sshd-configurator is not configured to depend on",
+        interface,
+    )
+    logger.error(
+        'ERROR: add rc_need="net.' + interface + '" to /etc/conf.d/sshd-configurator'
+    )
+    sys.exit(1)
 
 
 def warn_confd_sshd_configurator_interface(logger, interface):
     logger.error("\nERROR: SSHD_INTERFACE is not set in /etc/conf.d/sshd-configurator.")
-    logger.error("ERROR: add SSHD_INTERFACE=\"" + interface + "\" to /etc/conf.d/sshd-configurator")
-    quit(1)
+    logger.error(
+        'ERROR: add SSHD_INTERFACE="' + interface + '" to /etc/conf.d/sshd-configurator'
+    )
+    sys.exit(1)
 
 
 def run_checks(interface, logger):
     try:
-        with open('/etc/conf.d/sshd', 'r') as fh:
+        with open("/etc/conf.d/sshd", "r", encoding="utf8") as fh:
             fhr = filter(None, (line.strip() for line in fh))
-            fhr = filter(lambda row: row[0] != '#', fhr)
+            fhr = filter(lambda row: row[0] != "#", fhr)
             try:
-                result = [s for s in fhr if s.startswith('rc_need=')][-1]
+                result = [s for s in fhr if s.startswith("rc_need=")][-1]
             except IndexError:
                 warn_confd_sshd(logger=logger)
             else:
-                if 'sshd-configurator' not in result:
+                if "sshd-configurator" not in result:
                     warn_confd_sshd(logger=logger)
     except FileNotFoundError:
         warn_confd_sshd(logger=logger)
 
     listen_service = "net." + interface
     try:
-        with open('/etc/conf.d/sshd-configurator', 'r') as fh:
+        with open("/etc/conf.d/sshd-configurator", "r", encoding="utf8") as fh:
             fhr = filter(None, (line.strip() for line in fh))
-            fhr = filter(lambda row: row[0] != '#', fhr)
+            fhr = filter(lambda row: row[0] != "#", fhr)
             try:
-                result = [s for s in fhr if s.startswith('rc_need=')][-1]
+                result = [s for s in fhr if s.startswith("rc_need=")][-1]
             except IndexError:
                 warn_confd_sshd_configurator(interface=interface, logger=logger)
             else:
@@ -56,53 +69,70 @@ def run_checks(interface, logger):
         warn_confd_sshd_configurator(interface=interface, logger=logger)
 
     try:
-        with open('/etc/conf.d/sshd-configurator', 'r') as fh:
+        with open("/etc/conf.d/sshd-configurator", "r", encoding="utf8") as fh:
             fhr = filter(None, (line.strip() for line in fh))
-            fhr = filter(lambda row: row[0] != '#', fhr)
+            fhr = filter(lambda row: row[0] != "#", fhr)
             try:
                 result = [s for s in fhr if s.startswith("SSHD_INTERFACE=")][-1]
             except IndexError:
-                warn_confd_sshd_configurator_interface(interface=interface, logger=logger)
+                warn_confd_sshd_configurator_interface(
+                    interface=interface, logger=logger
+                )
             else:
                 if interface not in result:
-                    warn_confd_sshd_configurator_interface(interface=interface, logger=logger)
+                    warn_confd_sshd_configurator_interface(
+                        interface=interface, logger=logger
+                    )
     except FileNotFoundError:
         warn_confd_sshd_configurator_interface(interface=interface, logger=logger)
 
 
 @attr.s(auto_attribs=True)
-class SSHD_CONFIGURATOR():
+class SSHD_CONFIGURATOR:
     interface: str
     daemon: bool
     sshd_config: str
-    logger: logging
+    logger: logging.Logger
 
     def run(self):
         print("run")
         print("self.logger:", self.logger)
-        sshd_configurator_daemon(interface=self.interface, daemon=self.daemon, sshd_config=self.sshd_config, logger=self.logger)
+        sshd_configurator_daemon(
+            interface=self.interface,
+            daemon=self.daemon,
+            sshd_config=self.sshd_config,
+            logger=self.logger,
+        )
 
 
 @click.command()
-@click.argument('interface', nargs=1)
-@click.option('--daemon', is_flag=True)
-@click.option('--sshd-config', is_flag=False, default='/etc/ssh/sshd_config')
-def sshd_configurator(interface, daemon, sshd_config):
+@click.argument("interface", nargs=1)
+@click.option("--daemon", is_flag=True)
+@click.option("--sshd-config-file", is_flag=False, default="/etc/ssh/sshd_config")
+def sshd_configurator(interface, daemon, sshd_config_file):
     pidfile = "/run/sshd-configurator.pid"
     log = logging.getLogger(__name__)
     run_checks(interface=interface, logger=log)
-    #logger.setLevel(logging.DEBUG)
-    #logger.propagate = False
+    # logger.setLevel(logging.DEBUG)
+    # logger.propagate = False
     lfh = logging.StreamHandler()
     lfh.setLevel(logging.DEBUG)
     log.addHandler(lfh)
     keep_fds = [lfh.stream.fileno()]
 
     foreground = not daemon
-    sshd_configurator_obj = SSHD_CONFIGURATOR(interface=interface, daemon=daemon, sshd_config=sshd_config, logger=log)
-    daemon = Daemonize(app="ssh_configurator", pid=pidfile, action=sshd_configurator_obj.run, foreground=foreground, keep_fds=keep_fds)
+    sshd_configurator_obj = SSHD_CONFIGURATOR(
+        interface=interface, daemon=daemon, sshd_config=sshd_config_file, logger=log
+    )
+    daemon = Daemonize(
+        app="ssh_configurator",
+        pid=pidfile,
+        action=sshd_configurator_obj.run,
+        foreground=foreground,
+        keep_fds=keep_fds,
+    )
     daemon.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sshd_configurator()
--- sshd_configurator/sshd_configurator_daemon.py
+++ sshd_configurator/sshd_configurator_daemon.py
@@ -1,62 +1,65 @@
 #!/usr/bin/env python3
 
+import logging
 import os
 import sys
-#import logging
-import netifaces
-#import atexit
-#import signal
+from pathlib import Path
+# import atexit
+# import signal
 from time import sleep
-#logging.basicConfig(level=logging.DEBUG)
+from typing import Union
 
-'''
+import netifaces
+from eprint import eprint
+from pathtool import write_line_to_file
+
+# logging.basicConfig(level=logging.DEBUG)
+
+"""
     Parse sshd_config and make sure sshd is configured to start on the
     interface specified in /etc/conf.d/sshd-configurator
-'''
+"""
 
 
-def write_unique_line_to_file(line, file_to_write):
-    '''
-    Write line to file_to_write iff line not in file_to_write.
-    '''
-    assert isinstance(line, bytes)
-    assert line.count(b'\n') == 1
-    assert line.endswith(b'\n')
-    with open(file_to_write, 'rb+') as fh:
-        if line not in fh:
-            fh.write(line)
-
-
-def sshd_configurator_daemon(interface, daemon, sshd_config, logger):
+def sshd_configurator_daemon(
+    interface: str,
+    daemon: bool,
+    sshd_config: Path,
+    logger: logging.Logger,
+    verbose: Union[bool, int, float],
+):
     assert interface in netifaces.interfaces()
     assert os.path.getsize(sshd_config) != 0
-    listen_address = netifaces.ifaddresses(interface)[2][0]['addr']
+    listen_address = netifaces.ifaddresses(interface)[2][0]["addr"]
     assert listen_address
-    ssh_rule = 'ListenAddress ' + listen_address + ':22\n'
-    ssh_rule = ssh_rule.encode('utf8')
+    ssh_rule = "ListenAddress " + listen_address + ":22\n"
+    ssh_rule = ssh_rule.encode("utf8")
 
     try:
-        write_unique_line_to_file(line=ssh_rule, file_to_write=sshd_config)
+        write_line_to_file(line=ssh_rule, path=sshd_config, verbose=verbose)
     except PermissionError:
-        logger.error("ERROR: Unable to write to", sshd_config)
-        logger.error("ERROR: Run \"chattr -i " + sshd_config + "\"")
-        quit(1)
+        logger.error(f"ERROR: Unable to write to {sshd_config.as_posix()}")
+        logger.error(f'ERROR: Run "chattr -i {sshd_config.as_posix()} "')
+        sys.exit(1)
 
-    #def un_mute(*args):
+    # def un_mute(*args):
     #    command = "chattr -i " + sshd_config
     #    os.system(command)
 
     if os.geteuid() == 0:
-        #if not bool(getattr(sys, 'ps1', sys.flags.interactive)):
+        # if not bool(getattr(sys, 'ps1', sys.flags.interactive)):
         if daemon:
-            #command = "chattr +i " + sshd_config
-            #os.system(command)
-            #atexit.register(un_mute)
-            #signal.signal(signal.SIGTERM, un_mute)
-            #signal.signal(signal.SIGHUP, un_mute)
+            # command = "chattr +i " + sshd_config
+            # os.system(command)
+            # atexit.register(un_mute)
+            # signal.signal(signal.SIGTERM, un_mute)
+            # signal.signal(signal.SIGHUP, un_mute)
 
             while True:
                 sleep(1000000)
         else:
-            print("sshd appears to be properly configured to use", interface, file=sys.stderr)
-            quit(0)
+            eprint(
+                "sshd appears to be properly configured to use",
+                interface,
+            )
+            sys.exit(0)
